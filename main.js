713300fde87b83b729fcbf16741b186e73e0fa84
VEdant732 <VedantAjay377@gmail.com>
2025-01-20 20:43:25 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var d=prompt('password')
+if(d='H#LL!'){
+    console.log('It is a great password')
+    }
+    else{
+        console.log('It is not a great password')
+    }
+    else if(day='wednesday',,"thursday"){
+    console.log('its almost the weekend')
+    } 
+    else{
+        console.log('wait for the weekend')
+    }  
+
+
